99a1c4238edaba62c16b96d6b80b1dab45c07763
David Pendergast <dlp75@case.edu>
2016-09-25 17:17:25 -0400

    made dev-mode UI text render in the correct place

--- gamestate.py
+++ gamestate.py
@@ -196,32 +196,33 @@ class PlayingState(GameState):
         self._level_manager.load_level(self.level_num, self.player)
     
     def draw_gui(self, screen):
+        standard_width = HardSettings.standard_size()[0]
+        standard_height = HardSettings.standard_size()[1]
+        xoffset = (screen.get_width() - standard_width) / 2
+        yoffset = (screen.get_height() - standard_height) / 2
+        
+        if screen.get_width() > standard_width or screen.get_height() > standard_height:
+            # so that in dev mode you can see what the actual screen size would be.
+            pygame.draw.rect(screen,(255,0,0), pygame.Rect(xoffset,yoffset,standard_width,standard_height), 1)
+        
         level_text = self.font.render("Level: "+str(self.level_num + 1), True, (255, 255, 255))
         level_title = self.font.render(str(self._level_manager.current_level.name), True, (255, 255, 255))
         death_text = self.font.render("Deaths: "+str(self.death_count), True, (255, 255, 255))
         text_height = level_text.get_height()
         
         best_total_time = self._level_manager.get_best_run_time()
-        total_time_text_color = self.get_time_display_color(self.total_time, best_total_time, start_color=(255,255,255), end_color=(255,255,255))
+        total_time_text_color = self.get_time_display_color(self.total_time, best_total_time, start_color=(255, 255, 255), end_color=(255, 255, 255))
         total_time_text = self.font.render("Total: " + Utils.format_time(self.total_time), True, total_time_text_color)
         
         best_level_time = self._level_manager.get_best_level_time(self.level_num)
         level_time_text_color = self.get_time_display_color(self.level_time, best_level_time)
         level_time_text = self.font.render("Level: "+Utils.format_time(self.level_time), True, level_time_text_color)
         
-        screen.blit(level_text, (0, 0))
-        screen.blit(level_title, (0, text_height))
-        screen.blit(total_time_text, (screen.get_width()/2 - total_time_text.get_width()/2, 0))
-        screen.blit(level_time_text, (screen.get_width()/2 - level_time_text.get_width()/2, text_height))
-        screen.blit(death_text, (screen.get_width() - death_text.get_width(), 0))
-        
-        standard_width = HardSettings.standard_size()[0]
-        standard_height = HardSettings.standard_size()[1]
-        
-        if screen.get_width() > standard_width or screen.get_height() > standard_height:
-            xoffset = (screen.get_width() - standard_width) / 2
-            yoffset = (screen.get_height() - standard_height) / 2
-            pygame.draw.rect(screen,(255,0,0), pygame.Rect(xoffset,yoffset,standard_width,standard_height), 1)
+        screen.blit(level_text, (xoffset, yoffset))
+        screen.blit(level_title, (xoffset, yoffset + text_height))
+        screen.blit(total_time_text, (xoffset + standard_width/2 - total_time_text.get_width()/2, yoffset))
+        screen.blit(level_time_text, (xoffset + standard_width/2 - level_time_text.get_width()/2, yoffset + text_height))
+        screen.blit(death_text, (xoffset + standard_width - death_text.get_width(), yoffset))
             
     def get_time_display_color(self, current_time, best_time, start_color=(0, 255, 0), end_color=(255, 255, 100), fail_color=(255, 0, 0)):
         if best_time == None:
