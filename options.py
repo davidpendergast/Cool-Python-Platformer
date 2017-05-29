b7006c120213da21f3b77f4cca5ffe7fa4e6a2b5
David Pendergast <dlp75@case.edu>
2017-05-29 14:21:42 -0400

    fixed bug with set_resizable not respecting False

--- options.py
+++ options.py
@@ -23,7 +23,10 @@ def get_font(size):
 def set_resizable(val, size=None):
         if size == None:
             size = (0, 0) # leave size unchanged
-        pygame.display.set_mode(size, pygame.RESIZABLE)
+        if val:
+            pygame.display.set_mode(size, pygame.RESIZABLE)
+        else:
+            pygame.display.set_mode(size)
     
 _edgy_titles = [
         "Block Traversing Simulator 500",
